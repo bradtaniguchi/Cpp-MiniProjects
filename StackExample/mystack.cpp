9789cf02816399816410fbdbc68a6a5c47df0fb8
braddtaniguchi <bradtaniguchi@gmail.com>
2016-02-08 13:08:08 -0800

    Its working now!

--- StackExample/mystack.cpp
+++ StackExample/mystack.cpp
@@ -1,4 +1,43 @@
 //Bradley Taniguchi
 //New Version of Stack
+//Help gotten from: http://gauss.ececs.uc.edu/Courses/C321/html/stack++.html
+#include <iostream>
+
+class MyStack { //integer ONLY Stack
+    public: 
+        int pop();
+        void push(int x);
+        void printstack();
+        MyStack(int maxlength);
+    private:
+        int *s; //pointer to stack
+        int top; //index of stack
+        int maxelem;
+};
+
+MyStack::MyStack(int size) {
+    top = 0;
+    maxelem = size;
+    s = new int[maxelem];
+}
+int MyStack::pop() { //returns myitem
+    if (top == 0) return -1;
+    return s[--top];
+}
+void MyStack::push(int t) {
+    if (top == maxelem) return;
+    s[top++] = t;
+}
+void MyStack::printstack() {
+    if (top == 0) {
+        std::cout << "empty" << std::endl; 
+        return;
+    }
+    std::cout << "STACK: ";
+    for (int t=0; t < top; t++) {
+        std::cout << s[t] << " ";
+    }
+    std::cout << "\n";
+}
 
 
--- StackExample/stackex.cpp
+++ StackExample/stackex.cpp
@@ -9,10 +9,12 @@ int main(int argc, char* argv[]) {
     MyStack mystack(5); //new stack
     mystack.printstack();
     mystack.push(10);
-//    mystack.push(5);
+    mystack.push(5);
+    mystack.push(7);
     mystack.printstack();
-//    mystack.push(10);
-//    mystack.printstack();
-//    std::cout << "POP" << mystack.pop() << std::endl;
-//    mystack.printstack();
+    mystack.pop();
+    mystack.pop();
+    mystack.printstack();
+    mystack.pop();
+    mystack.printstack();
 }
